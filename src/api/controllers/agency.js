ef89899bb4a421a8c0a08a9b14e4d2256c8e47e4
Shoxrux Rashidov <hi.shoxrux@gmail.com>
2020-09-15 15:37:26 +0300

    Adding Agency Controller

---
+++ src/api/controllers/agency.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Order = require("../models/order");
+const Product = require("../models/product");
+
+exports.create = (req, res, next) => {
+
+};
+
+exports.show = (req, res, next) => {
+
+};
+
+exports.edit = (req, res, next) => {
+
+};
+
+exports.delete = (req, res, next) => {
+
+};
+
+exports.getAll = (req, res, next) => {
+
+};
